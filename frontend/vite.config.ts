4dcc1f4cc1fab169bfc2108d66451dc009408d03
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2024-12-14 19:09:44 +0000

    fix(vercel): Added new vercel.json configuration and added typescript @ configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/convert/': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'), // Configura el alias @ a src
+        },
     },
-  },
+    server: {
+        proxy: {
+            '/convert/': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+            },
+        },
+    },
 });
